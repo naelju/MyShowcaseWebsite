4d7eddc090d3bd0afcf73eb43f6a04acfbb61c7a
Naël <nael.juston@etu.univ-lyon1.fr>
2022-01-25 01:03:00 +0100

    first night of coding

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -8,6 +9,26 @@ class DefaultController extends AbstractController
 {
     public function index(): Response
     {
-        return new Response("Hello wolrd!");
+        $date= new DateTime('now');
+        $date=$date->format('Y-n-j');
+        $date=explode('-',$date);
+        $year=$date[0];
+        $month=$date[1];
+        $day=$date[2];
+        $age=$year-2002;
+        if($month<11){
+            $age--;
+        }elseif($month==11){
+            if($day<4){
+                $age--;
+            }
+        }
+        
+ 
+        
+        return $this->render('homepage.html.twig', ['age'=>$age]);
     }
+
+    
+
 }
