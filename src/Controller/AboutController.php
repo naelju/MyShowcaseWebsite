165b25ed2d0b9bec8b965c6b8257676501ff8789
Naël Juston <74049089+naelju@users.noreply.github.com>
2022-03-09 13:54:40 +0100

    homepage + about

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AboutController extends AbstractController
+{
+    public function index(): Response
+    {
+        $date= new DateTime('now');
+        $date=$date->format('Y-n-j');
+        $date=explode('-',$date);
+        $year=$date[0];
+        $month=$date[1];
+        $day=$date[2];
+        $age=$year-2002;
+        if($month<11){
+            $age--;
+        }elseif($month==11){
+            if($day<4){
+                $age--;
+            }
+        }
+        
+ 
+        
+        return $this->render('about.html.twig', ['age'=>$age]);
+    }
+
+    
+
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,26 +9,6 @@ class DefaultController extends AbstractController
 {
     public function index(): Response
     {
-        $date= new DateTime('now');
-        $date=$date->format('Y-n-j');
-        $date=explode('-',$date);
-        $year=$date[0];
-        $month=$date[1];
-        $day=$date[2];
-        $age=$year-2002;
-        if($month<11){
-            $age--;
-        }elseif($month==11){
-            if($day<4){
-                $age--;
-            }
-        }
-        
- 
-        
-        return $this->render('homepage.html.twig', ['age'=>$age]);
+        return $this->render('homepage.html.twig');
     }
-
-    
-
 }
