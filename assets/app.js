b8d5764756291d4b11868c911eceb2d336e128af
Naël Juston <74049089+naelju@users.noreply.github.com>
2022-05-03 20:48:05 +0200

    Stylizing and rewriting on about page

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import './styles/menuBar.css';
+import './styles/aboutMe.css';
+import './styles/rubiks.css';
 
 // start the Stimulus application
 import './bootstrap';
